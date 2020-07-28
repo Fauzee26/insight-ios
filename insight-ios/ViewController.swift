b41408b8a04421c85c20640cfc8a44ed1dca5e52
Fauzi <hilmy2602@gmail.com>
2020-07-28 22:45:03 +0700

    Initial Commit

---
+++ insight-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  insight-ios
+//
+//  Created by Muhammad Hilmy Fauzi on 28/07/20.
+//  Copyright © 2020 Muhammad Hilmy Fauzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
