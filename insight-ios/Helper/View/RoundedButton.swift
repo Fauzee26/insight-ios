6801ab978ed9dd1226893a4b44544b09754437ac
Fauzi <hilmy2602@gmail.com>
2020-08-02 11:46:50 +0700

    organizing folders and auth design

---
+++ insight-ios/Helper/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  insight-ios
+//
+//  Created by Muhammad Hilmy Fauzi on 02/08/20.
+//  Copyright © 2020 Muhammad Hilmy Fauzi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
