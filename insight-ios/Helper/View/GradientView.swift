71221344f00edf243e54ef1e582a4b75be3aa0ba
Fauzi <hilmy2602@gmail.com>
2020-08-02 14:47:56 +0700

    UI - Layout Design for Tab Bar

---
+++ insight-ios/Helper/View/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  insight-ios
+//
+//  Created by Muhammad Hilmy Fauzi on 02/08/20.
+//  Copyright © 2020 Muhammad Hilmy Fauzi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = UIColor(named: "light-green")! {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1647058824, green: 0.6980392157, blue: 0.7333333333, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
