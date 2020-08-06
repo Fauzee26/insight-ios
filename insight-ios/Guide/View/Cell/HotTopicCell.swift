8110ca90335e5c7594aaa5ca8a329393acbef63d
Fauzi <hilmy2602@gmail.com>
2020-08-06 09:29:06 +0700

    add podfile

---
+++ insight-ios/Guide/View/Cell/HotTopicCell.swift
@@ -0,0 +1,18 @@
+//
+//  HotTopicCell.swift
+//  insight-ios
+//
+//  Created by Muhammad Hilmy Fauzi on 06/08/20.
+//  Copyright © 2020 Muhammad Hilmy Fauzi. All rights reserved.
+//
+
+import UIKit
+
+class HotTopicCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
